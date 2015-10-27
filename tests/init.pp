651f5d51838b863da05b30e2a058415063992405
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2015-10-26 22:30:34 -0400

    fix wrong class name used in test

--- tests/init.pp
+++ tests/init.pp
@@ -9,7 +9,7 @@
 # Learn more about module testing here:
 # http://docs.puppetlabs.com/guides/tests_smoke.html
 #
-class { 'huskycode-vim' :
+class { 'vim' :
   user         => 'vagrant',
   home_dir     => '/home/vagrant',
   plugin1_name => 'nerdtree',
